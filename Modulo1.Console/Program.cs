51a8622b0cb2a740cfdd2f76659714bbc0e937b5
Brunella Martina Doege <brunella.doege@ambevtech.com.br>
2024-05-15 10:32:48 -0300

    Adicionar arquivos de projeto.

---
+++ Modulo1.Console/Program.cs
@@ -0,0 +1,14 @@
+﻿// Um código onde voce pergunta para o usuário o nome dele, a idade, o cargo e no final imprime uma saudação na tela com todas as infos e com data e hora
+
+Console.Write("Nome: ");
+string nome = Console.ReadLine();
+
+Console.Write("Idade: ");
+int idade = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Cargo: ");
+string cargo = Console.ReadLine();
+
+Console.WriteLine($"\nOlá {nome}, você tem {idade} anos e é {cargo}. Hoje é dia {DateTime.Now} ");
+
+Console.ReadKey();
